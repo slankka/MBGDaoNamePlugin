0b4257ab3a2249aacc5b6230d93ddec356dbf484
slankka <cor_twi@outlook.com>
2016-01-22 16:57:48 +0800

    add Mapper Id name change features

--- src/com/slanka/mybatis/plugin/MyBatisGeneratorDaoNamePlugin.java
+++ src/com/slanka/mybatis/plugin/MyBatisGeneratorDaoNamePlugin.java
@@ -6,7 +6,7 @@ import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 /**
  * 
- * Mybatis插件 自动把Dao接口名称改成xxxDao
+ * Mybatis插件 自动把Dao接口以及把带有Example的名称改成xxxDao
  * @author zz 2016年1月18日 16:29:56
  *
  */
@@ -21,9 +21,20 @@ public class MyBatisGeneratorDaoNamePlugin extends PluginAdapter{
 		// TODO Auto-generated method stub
 		super.initialized(introspectedTable);
 		String name = introspectedTable.getMyBatis3JavaMapperType();
-		/**
-		 * 不要试图修改selectByExample
-		 */
+		
+		introspectedTable.setCountByExampleStatementId("countByCriteria"); //$NON-NLS-1$
+        introspectedTable.setDeleteByExampleStatementId("deleteByCriteria"); //$NON-NLS-1$
+        introspectedTable.setInsertSelectiveStatementId("insertSelective"); //$NON-NLS-1$
+        introspectedTable.setSelectByExampleStatementId("selectByCriteria"); //$NON-NLS-1$
+        introspectedTable.setSelectByExampleWithBLOBsStatementId("selectByCriteriaWithBLOBs"); //$NON-NLS-1$
+        introspectedTable.setUpdateByExampleStatementId("updateByCriteria"); //$NON-NLS-1$
+        introspectedTable.setUpdateByExampleSelectiveStatementId("updateByCriteriaSelective"); //$NON-NLS-1$
+        introspectedTable.setUpdateByExampleWithBLOBsStatementId("updateByCriteriaWithBLOBs"); //$NON-NLS-1$
+        introspectedTable.setExampleWhereClauseId("Criteria_Where_Clause"); //$NON-NLS-1$
+        introspectedTable.setMyBatis3UpdateByExampleWhereClauseId("Update_By_Criteria_Where_Clause"); //$NON-NLS-1$
+		
+		//RenamePlugin Can Change This :Example->Criteria  
+		//introspectedTable.setExampleType(exampleType);
 		introspectedTable.setMyBatis3JavaMapperType(name.replaceAll("Mapper$", "Dao"));
 		
 	}
